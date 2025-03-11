bdc81b7dbc9ba3741ba9de0c29295e4a45d92322
Luiz A D R Marques <stg@stgsys.com>
2025-03-11 14:47:44 -0300

    Implement form validation for event creation and update event details

--- EventSite/Services/Event.cs
+++ EventSite/Services/Event.cs
@@ -2,12 +2,21 @@ using System;
 
 namespace EventSite.Services
 {
-    public class Event
-    {
-        public int Id { get; set; } = 0;
-        public string Name { get; set; } = string.Empty;
-        public DateTime Date { get; set; } = DateTime.Today;
-        public string Description { get; set; } = string.Empty;
-        public string Location { get; set; } = string.Empty;
-    }
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+
+        public class Event
+        {
+            public int Id { get; set; } = 0;
+            [Required]
+            public string Name { get; set; } = string.Empty;
+            [Required]
+            [DataType(DataType.Date)]
+            public DateTime Date { get; set; } = DateTime.Today;
+            public string Description { get; set; } = string.Empty;
+            [Required]
+            public string Location { get; set; } = string.Empty;
+        }
+    
 }
